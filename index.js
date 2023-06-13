fd343bc1292b5cf920ec597de6b91cddde01a172
Taha <tahaozcilingir@msn.com>
2023-06-13 13:10:46 -0400

    uses functions and function callback

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42)
+}
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue)
+    return blocks * 264
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance <= 2000) {
+        const farePrice = (distance - 400) * 0.02
+        return farePrice
+    
+    
+    } else if (distance <= 2500) {
+        return 25;
+    }
+    else if (distance >= 2500) {
+
+        return 'cannot travel that far';
+    }
+}
+
+ 
